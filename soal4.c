47da396cadc256a2eb4a07e997bcbaa02ad0f604
Unknown <m.hazdik@gmail.com>
2017-10-24 20:18:38 +0700

    soal4
    soal4

--- soal4.c
+++ soal4.c
@@ -1,24 +1,35 @@
-#include<stdio.h>
-#include<pthread.h>
-#include<stdlib.h>
-#include<unistd.h>
+# include <stdio.h>
+# include <pthread.h>
 
-pthread_t t;
-int num;
-void* factorial()
-{
-	int fac=1;
-	int a;
-	for(a=1;a<=num;a++) 
-	{
-		fac=fac*a;
-		printf("Factorial of Given Number is = %d", fac);
-	}
-}
+void * thread_sum(void *);
+int TotalSum=1;
+pthread_mutex_t mVar=PTHREAD_MUTEX_INITIALIZER;
 
 int main()
 {
-	printf(" Enter Number To Find Its Factorial: ");
-	scanf("%d", &num);
-	pthread_create (&t,NULL,&factorial,(void*)&num);return 0;
+    int iNumber,iCount;
+    pthread_t tid;
+    printf("Enter Number Up to Which You want to Sum :");
+    scanf("%d",&iNumber);
+    pthread_create(&tid,NULL,thread_sum,(void *)&iNumber);
+
+    pthread_join(tid,NULL);
+
+    printf("Final Sum is : %d \n",TotalSum);
+//    return 0;
+}
+
+void *thread_sum(void *no)
+{
+    int *iNumber,iCount;
+    iNumber=(int*)no;
+
+    for(iCount=1;iCount<=*iNumber;iCount=iCount+1)
+    {
+        pthread_mutex_lock(&mVar);
+        TotalSum=TotalSum * iCount;
+        pthread_mutex_unlock(&mVar);
+	printf("%d\n", TotalSum);
+    }
+    pthread_exit(NULL);
 }
