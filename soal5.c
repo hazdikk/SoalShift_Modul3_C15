16639fba2bac6c5ecd394bd40e303926db14bc94
Unknown <m.hazdik@gmail.com>
2017-10-25 16:47:50 +0700

    Auto stash before merge of "master" and "origin/master"
    gatau salah dimana, strcpy sama strstr kerja

--- soal5.c
+++ soal5.c
@@ -6,23 +6,31 @@
 #include <string.h>
 
 pthread_t t[50];
+int status=0;
 
 void *cari(void *arg){
+	status++;
 	FILE* f;
 	f=fopen("Novel.txt", "r");
 	char search[100];
 	int flag;
 	char novel[100];
+	strcpy(search, arg);
+	printf("%s\n", search);
 	while(fscanf(f, "%s", novel)!=EOF){
+		if(strstr(novel, search) != NULL)	flag++;
 	}
+	printf("%s : %d", search, flag);
 }
 
 int main(int argc, char *argv[]){
 	int i;
-	char asu[100] = "gue ganteng";
-	char ggwp[100] = "ganteng";
-	printf("%s\n", strstr(asu, ggwp));
 	for(i=1; i<=argc; i++){
 		pthread_create(&(t[i]), NULL, &cari, (void*)argv[i]);
+		while(status==0){
+		}
+	}
+	for(i=1; i<=argc; i++){
+		pthread_join(t[i], NULL);
 	}
 }
