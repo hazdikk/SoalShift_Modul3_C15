a482f41dbd776571e86c34ff9aace2365a32f5e8
afrianmc <33081201+afrianmc@users.noreply.github.com>
2017-10-25 15:50:24 +0700

    inshaalloh fix, inshaalloh tapi

--- soal3.c
+++ soal3.c
@@ -1,29 +1,70 @@
-# include <stdio.h>
-# include <pthread.h>
-# include <unistd.h>
-# include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int lohan;
-int kepiting;
+int stats[2];
+void *kepiting () {
+	while (1) {
 
-pthread_t t_k, t_l;
-
-void* status(){
-        printf("lalala");
-        while(1){
-                if(lohan <= 0 || lohan > 100 || kepiting <= 0 || kepiting > 100){
-                        printf("Game Selesai");
-                        exit(EXIT_FAILURE);
-                }
-                lohan = 103;
-        }
+		sleep(20);
+		stats[0] -= 10;
+		if (stats[0] <= 0 || stats [0] > 100) {
+			exit(EXIT_FAILURE);
+		}
+	}
 }
+void *lohan () {
+	while (1) {
 
-int menu();
+			sleep(20);
+			stats[1] -= 10;
+			if (stats[1] <= 0 || stats [1] > 100) {
+				exit(EXIT_FAILURE);
+			}
+		}
+}
+void makan () {
+	printf("hewan yang di berimakan\n1. kepiting\n2. Lohan\n");
+	int a;
+	scanf ("%d", &a);
+	if (a==1) {
+		stats [0]+=10;
+		printf("Status kepiting bertambah %d\n", stats[0]);
+	}
+	else if (a==2) {
+		stats [1]+=10;
+		printf("Status lohan bertambah %d\n", stats[1]);
+	}
+	
+}
+int main () 	{
+	stats [0]=100;
+	stats [1]=100;
+	pthread_t kepiting_t, lohan_t;
+	pthread_create (&kepiting_t,NULL,&kepiting,NULL);	
+	pthread_create (&lohan_t,NULL,&lohan,NULL);
 
-int main(){
-     lohan=100, kepiting=100;
-     int menu;
-     pthread_create(&t_k, NULL, status, NULL);
-     pthread_create(&t_l, NULL, status, NULL);
+	while (1) {
+		int pil;
+		if(stats[1] <= 0 || stats[1] > 100 || stats[0] <= 0 || stats[0] > 100) {
+			printf("Status lohan = %d\n", stats[0]);
+			printf("Status kepiting = %d\n", stats[1]);
+			printf("GAME OVER\n");
+			exit(EXIT_FAILURE);
+		}
+		else {
+			printf("1. Lihat status\n2. Berimakan\n");
+			scanf ("%d",&pil);
+
+			if (pil==1) {
+				printf("Status Kepiting: %d\n", stats[0]);
+				printf("Status Lohan: %d\n", stats[1]);
+			}
+			else if (pil==2) {
+				makan();
+			}
+		}
+}
 }
