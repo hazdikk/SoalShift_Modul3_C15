f1bb5e3f39bbfad3842a15daea99355cbe820c10
Unknown <m.hazdik@gmail.com>
2017-10-25 16:26:18 +0700

    nyicil fungsi buat nyari kata

--- soal5.c
+++ soal5.c
@@ -3,10 +3,19 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 pthread_t t[50];
 
-void *cari(void *);
+void *cari(void *arg){
+	FILE* f;
+	f=fopen("Novel.txt", "r");
+	char search[100];
+	int flag;
+	char novel[100];
+	while(fscanf(f, "%s", novel)!=EOF){
+	}
+}
 
 int main(int argc, char *argv[]){
 	int i;
