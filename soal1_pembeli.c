f154857923a1b3c39e18262141985ca0770a2de1
afrianmc <33081201+afrianmc@users.noreply.github.com>
2017-10-25 17:13:00 +0700

    kayanya masih ada yang salah

--- soal1_pembeli.c
+++ soal1_pembeli.c
@@ -17,7 +17,7 @@ int main(){
 key_t sharedKEY;
 int sharedID;
 
-struct Memory barang;
+struct Memory *barang;
 int menu;
 char nama_barang[101];
 int total;
@@ -31,14 +31,14 @@ exit(1);
 }
 
 barang = (struct Memory *) shmat(sharedID, NULL, 0);
-barang.status = BELUM_SIAP;
+barang->status = BELUM_SIAP;
 
-barang.MP4A1=0;
-barang.PM2_V1=0;
-barang.SPR_3=0;
-barang.SS2_V5=0;
-barang.SPG1_V3=0;
-barang.MINE=0;
+barang->MP4A1=0;
+barang->PM2_V1=0;
+barang->SPR_3=0;
+barang->SS2_V5=0;
+barang->SPG1_V3=0;
+barang->MINE=0;
 
 while(1)
 {
@@ -50,12 +50,12 @@ scanf("%d", &menu);
 
 if (menu == 1)
 {
-printf("Jumlah MP4A1 = %d\n", barang.MP4A1);
-printf("Jumlah PM2-V1 = %d\n", barang.PM2_V1);
-printf("Jumlah SPR-3 = %d\n", barang.SPR_3);
-printf("Jumlah SS2-V5 = %d\n", barang.SS2_V5);
-printf("Jumlah SPG1-V3 = %d\n", barang.SPG1_V3);
-printf("Jumlah MINE = %d\n", barang.MINE);
+printf("Jumlah MP4A1 = %d\n", barang->MP4A1);
+printf("Jumlah PM2-V1 = %d\n", barang->PM2_V1);
+printf("Jumlah SPR-3 = %d\n", barang->SPR_3);
+printf("Jumlah SS2-V5 = %d\n", barang->SS2_V5);
+printf("Jumlah SPG1-V3 = %d\n", barang->SPG1_V3);
+printf("Jumlah MINE = %d\n", barang->MINE);
 }
 
 else if(menu == 2){
@@ -66,58 +66,58 @@ printf(">> ");
 scanf("%s %d", nama_barang, &total);
 
 if (strcmp(nama_barang,"MP4A1")==0){
- if (total > barang.MP4A1){
+ if (total > barang->MP4A1){
  printf("Barang di stock tidak cukup\n");
  }
  else{
- barang.MP4A1 -= total;
+ barang->MP4A1 -= total;
  }
 }
 
 if (strcmp(nama_barang,"PM2-V1")==0){
- if (total > barang.PM2_V1){
+ if (total > barang->PM2_V1){
  printf("Barang di stock tidak cukup\n");
  }
  else{
- barang.PM2_V1 -= total;
+ barang->PM2_V1 -= total;
  }
 
 }
 
 if (strcmp(nama_barang,"SPR-3")==0){
- if (total > barang.SPR_3){
+ if (total > barang->SPR_3){
  printf("Barang di stock tidak cukup\n");
  }
  else{
- barang.SPR_3 -= total;
+ barang->SPR_3 -= total;
  }
 }
 
 if (strcmp(nama_barang,"SS2-V5")==0){
- if (total > barang.SS2_V5){
+ if (total > barang->SS2_V5){
  printf("Barang di stock tidak cukup\n");
  }
  else{
- barang.SS2_V5 -= total;
+ barang->SS2_V5 -= total;
  }
 }
 
 if (strcmp(nama_barang,"SPG1-V3")==0){
- if (total > barang.SPG1_V3){
+ if (total > barang->SPG1_V3){
  printf("Barang di stock tidak cukup\n");
  }
  else{
- barang.SPG1_V3 -= total;
+ barang->SPG1_V3 -= total;
  }
 
 }
 
 if (strcmp(nama_barang,"MINE")==0){
- if (total > barang.MINE){
+ if (total > barang->MINE){
  printf("Barang di stock tidak cukup\n");
  }
  else{
- barang.MINE -= total;
+ barang->MINE -= total;
  }
 }
 
