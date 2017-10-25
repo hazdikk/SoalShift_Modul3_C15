f3e3998a180f2a2504c5d1bc78475421ecc163f8
afrianmc <33081201+afrianmc@users.noreply.github.com>
2017-10-25 16:07:21 +0700

    edit lagi

--- soal1_penjual.c
+++ soal1_penjual.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <string.h>
+#include <sys/stat.h>
+#define BELUM_SIAP 0
+#define SIAP 1
 
-void main()
-{
+struct Memory{
+int MP4A1, PM2_V1, SPR_3, SS2_V5, SPG1_V3, MINE, status;
+};
 
-}
 
