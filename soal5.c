745a23e47858c02c384e7c702b551a36ad855a0e
Unknown <m.hazdik@gmail.com>
2017-10-25 17:01:46 +0700

    Auto stash before merge of "master" and "origin/master"
    DONEEE

--- soal5.c
+++ soal5.c
@@ -9,26 +9,23 @@ pthread_t t[50];
 int status=0;
 
 void *cari(void *arg){
-	status++;
 	FILE* f;
 	f=fopen("Novel.txt", "r");
 	char search[100];
-	int flag;
+	int flag=0;
 	char novel[100];
 	strcpy(search, arg);
-	printf("%s\n", search);
 	while(fscanf(f, "%s", novel)!=EOF){
 		if(strstr(novel, search) != NULL)	flag++;
 	}
-	printf("%s : %d", search, flag);
+	fclose(f);
+	printf("%s : %d\n", search, flag);
 }
 
 int main(int argc, char *argv[]){
 	int i;
 	for(i=1; i<=argc; i++){
 		pthread_create(&(t[i]), NULL, &cari, (void*)argv[i]);
-		while(status==0){
-		}
 	}
 	for(i=1; i<=argc; i++){
 		pthread_join(t[i], NULL);
