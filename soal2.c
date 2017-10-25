f13db976b8a4149508490275db5ed763251fadb7
afrianmc <33081201+afrianmc@users.noreply.github.com>
2017-10-25 17:36:49 +0700

    masih eror

--- soal2.c
+++ soal2.c
@@ -6,6 +6,31 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+typedef struct game{
+    int lubang1[17];
+    int lubang2[17];
+}hole;
+
+pthread_t tid1;
+pthread_t tid2;
+hole ho;
+
+void* playandcount(void *arg)
+{
+    int i;
+    pthread_t id = pthread_self();
+
+       	if(pthread_equal(id,tid1)){
+	   
+    	}
+	else if(pthread_equal(id,tid2){
+	   
+    	}
+    
+    return NULL;
+}
+
+
 int main()
 {
     
